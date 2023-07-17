8fcb5c577c3c271686df3c355041dbc26f658fa7
Kyle Blunt <kbluntaudio@gmail.com>
2023-07-16 21:16:01 -0500

    Created options to display, add, adopt, and remove plants

--- Plant.cs
+++ Plant.cs
@@ -1,9 +1,9 @@
 public class Plant
 {
-    public string Species { get; set; }
+    public string? Species { get; set; }
     public int LightNeeds { get; set; }
     public decimal AskingPrice { get; set; }
-    public string City { get; set; }
-    public string ZIP { get; set; }
+    public string? City { get; set; }
+    public string? ZIP { get; set; }
     public bool IsSold { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@
     },
     new Plant()
     {
-        Species = "Aloe Vera",
+        Species = "Ficus",
         LightNeeds = 2,
         AskingPrice = 5.00m,
         City = "Chicago",
@@ -51,7 +51,10 @@ string greeting = @"Welcome to ExtraVert
 Your one-stop shop for buying plants you won't take care of!";
 
 Console.WriteLine(greeting);
-string choice = null;
+string? choice = null;
+
+// MAIN MENU 
+
 while (choice != "0")
 {
     Console.WriteLine(@"Please choose an option:
@@ -74,18 +77,25 @@ while (choice != "0")
     }
     else if (choice == "2")
     {
-        throw new NotImplementedException("Post plant for adoption");
+        PostPlant();
+        Console.WriteLine("Press any key to continue:");
+        Console.ReadKey();
     }
     else if (choice == "3")
     {
-        throw new NotImplementedException("Adopt a plant");
+        AdoptPlant();
+        Console.WriteLine("Press any key to continue:");
+        Console.ReadKey();
     }
     else if (choice == "4")
     {
-        throw new NotImplementedException("Delist a plant");
+        RemovePlant();
+        Console.WriteLine("Press any key to continue:");
+        Console.ReadKey();
     }
 }
 
+// DISPLAY ALL PLANTS
 
 void ViewAllPlants() {
 
@@ -96,14 +106,14 @@ void ViewAllPlants() {
         Console.WriteLine($"{i + 1}. {plant.Species}");
     }
 
-    Plant chosenPlant = null;
+    Plant chosenPlant = null!;
 
     while (chosenPlant == null)
     {
         Console.WriteLine("Please enter a plant number:");
         try
         {
-            int response = int.Parse(Console.ReadLine().Trim());
+            int response = int.Parse(Console.ReadLine()!.Trim());
             chosenPlant = plants[response - 1];
         }
         catch (FormatException)
@@ -117,10 +127,123 @@ void ViewAllPlants() {
         catch (Exception ex)
         {
             Console.WriteLine(ex);
-            Console.WriteLine("Not valid choice. Please try again.");
+            Console.WriteLine("Not a valid choice. Please try again.");
         }
     }
 
-    Console.WriteLine(@$"You chose:
-{chosenPlant.Species}, which costs ${chosenPlant.AskingPrice}.");
+    Console.WriteLine(@$"A {chosenPlant.Species} in {chosenPlant.City} {(chosenPlant.IsSold ? "was sold" : "is available" )} for ${chosenPlant.AskingPrice}.");
+}
+
+// POST PLANT FOR ADOPTION
+
+void PostPlant()
+{
+    Console.WriteLine("Add a new plant");
+    Console.WriteLine("Enter the species of the plant:");
+    string? species = Console.ReadLine()!;
+
+    Console.WriteLine("Enter the light needs of the plant using numbers 1-5:");
+    int lightNeeds = int.Parse(Console.ReadLine()!.Trim());
+
+    Console.WriteLine("Enter the price of the plant:");
+    decimal askingPrice = decimal.Parse(Console.ReadLine()!.Trim());
+
+    Console.WriteLine("Enter the city:");
+    string? city = Console.ReadLine()!;
+    
+    Console.WriteLine("Enter the zip code:");
+    string? zipCode = Console.ReadLine();
+
+    Plant plant = new Plant
+    {
+        Species = species,
+        LightNeeds = lightNeeds,
+        AskingPrice = askingPrice,
+        City = city,
+        ZIP = zipCode
+    };
+
+    plants.Add(plant);
+
+    Console.WriteLine("You've successfully added a plant!");
+}
+
+void AdoptPlant()
+{
+    Console.WriteLine("Choose a plant to adopt:");
+
+    List<Plant> adoptablePlants = plants.Where(plant => !plant.IsSold).ToList();
+
+    Console.WriteLine("Plants available for adoption:");
+
+    for (int i = 0; i < adoptablePlants.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {adoptablePlants[i].Species}");
+    }
+
+    Plant adoptedPlant = null!;
+
+    while (adoptedPlant == null)
+    {
+        Console.WriteLine("Please enter a plant number to adopt:");
+        try
+        {
+            int response = int.Parse(Console.ReadLine()!.Trim());
+            adoptedPlant = adoptablePlants[response - 1];
+            adoptedPlant.IsSold = true;
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Please choose the correct number!");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Please choose an existing plant only!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            Console.WriteLine("Not a valid choice. Please try again.");
+        }
+    }
+    Console.WriteLine($"You've successfully adopted a {adoptedPlant.Species} plant!");
+}
+
+void RemovePlant()
+{
+    Console.WriteLine("Choose a plant to remove:");
+
+    for (int i = 0; i < plants.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {plants[i].Species}");
+    }
+
+    Plant RemovePlant = null!;
+
+    while (RemovePlant == null)
+    {
+        try
+        {
+            Console.WriteLine("Enter plant number to remove:");
+            int response = int.Parse(Console.ReadLine()!.Trim());
+            RemovePlant = plants[response - 1];
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Please enter a valid number from list.");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Please enter a valid number from list.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            Console.WriteLine("Not a valid choice. Please try again.");
+        }
+    }
+
+    plants.Remove(RemovePlant);
+
+    Console.WriteLine($"You've removed a {RemovePlant.Species} plant!");
 }
